e3e7019a83270f3e1145963ee304482ee4b90924
Nils Thevenard <nils.thevenard@gmail.com>
2023-02-20 11:32:16 +1300

    first branch

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-"use strict";
+"use strict"; //this activates strict mode
+// when strict mode activated the console
+//will inform us of any possible issues or why things dont work
 //strict mode
 const interface = "audio";
 const public = "audence";
 console.log(interface);
 console.log(public);
+
+let hasDriverLIcence;
+
+//addition on new branch
